82e025599cf0cf31ac724f9bcff698f8b39d4bf4
Omkar Kabde <omkarkabde@gmail.com>
2024-08-20 10:51:25 +0530

    Update fcfs.py

--- fcfs.py
+++ fcfs.py
@@ -1,29 +1,32 @@
-def findWT(processes, n, bt):
+def findWT(bt):
+    n = len(bt)
     wt = [0]*n
 
     for i in range(1, n):
         wt[i] = bt[i-1] + wt[i-1]
     return wt
 
-def findTAT(processes, n, bt, wt):
+def findTAT(bt, wt):
+    n = len(bt)
     tat = [0]*n
 
     for i in range(n):
         tat[i] = bt[i] + wt[i]
     return tat
 
-def findavgTime(processes, n, bt):
+def findavgTime(processes, bt):
+    n = len(processes)
     total_wt, total_tat = 0, 0
 
-    wt = findWT(processes, n, bt)
-    tat = findTAT(processes, n, bt, wt)
+    wt = findWT(bt)
+    tat = findTAT(bt, wt)
 
     print("  Pid---BT---WT---TAT")
 
     for i in range(n):
-        total_wt = total_wt + wt[i]
-        total_tat = total_tat + tat[i]
-        print(f" {str(i+1)}   {str(bt[i])}   {str(wt[i])}  {str(tat[i])}")
+        total_wt += wt[i]
+        total_tat += tat[i]
+        print(f" {i+1}  {bt[i]}   {wt[i]}  {tat[i]}")
 
     print(f"Average WT = {str(total_wt/n)}")
     print(f"Average TAT = {str(total_tat/n)}")
@@ -33,5 +36,5 @@ processes = [1,2,3]
 n = len(processes)
 bt = [10, 5, 8]
 
-findavgTime(processes, n, bt)
+findavgTime(processes, bt)
 
